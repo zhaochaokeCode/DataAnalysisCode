1394f441b93a0c7cd320a191d7707442de170644
Zhaochaoke <308471666@qq.com>
2017-01-23 19:19:17 +0800

    check table

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -74,6 +74,8 @@ class SiteController extends Controller
         $current_dir = opendir($path);
         $logType = array();
         $ret = array();
+
+
         //获取mysql的表名
         $sql= "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'data_analysis'";
         $command=Yii::$app->db->createCommand($sql);
@@ -81,6 +83,8 @@ class SiteController extends Controller
         foreach($posts as $v){
             $tabArr[] =$v['TABLE_NAME'] ;
         }
+
+
         while (($file = readdir($current_dir)) !== false) {
             if ($file == '.' || $file == '..') {
                 continue;
@@ -89,28 +93,39 @@ class SiteController extends Controller
                 $fileName = $path . '/' . $file;
                 $cont = file_get_contents($fileName);
                 $datas = explode("\n", $cont);
+
                 foreach ($datas as $v) {
                     if ($json = json_decode($v)) {
+                        $tmpData = $this->objeToArr($json) ;
+                        $sqlData = $this->createSqlData($tmpData);
+
+                        continue ;
                         $tmp = array_keys($logType);
 
-                        if (isset($json->f_log_name)) {
-                            if (!in_array($json->f_log_name, $tmp)) {
-                                $logType[$json->f_log_name] = 1;
-                                $ret[] = array($json->f_log_name, $fileName);
-                                $type[] = $v ;
+                        if (($name=$useData['f_log_name'])) {
+                            if($name=='log_online_character_cnt') $name='log_onlineinfo';
+                            $tabName ='bi_'.$name ;
 
-                                if($json->f_log_name=='log_online_character_cnt'){
-                                    $json->f_log_name= 'log_onlineinfo' ;
-                                }
-                                if(in_array($tabName = 'bi_'.$json->f_log_name,$tabArr)){
-                                    echo 'in '.$json->f_log_name."<br/>"."<br/>"  ;
-                                }else{
-                                    echo '------ not in'.$json->f_log_name."<br/>"."<br/>"  ;
-                                }
 
-                            } else {
-                                $logType[$json->f_log_name]++;
-                            }
+
+//                            if (!in_array($tabName, $tmp)) {
+//                                $logType[$json->f_log_name] = 1;
+//                                $ret[] = array($json->f_log_name, $fileName);
+//                                $type[] = $v ;
+//
+//                                if($json->f_log_name=='log_online_character_cnt'){
+//                                    $json->f_log_name= 'log_onlineinfo' ;
+//                                }
+//                                if(in_array($tabName = 'bi_'.$json->f_log_name,$tabArr)){
+//                                    echo 'in '.$json->f_log_name."<br/>"."<br/>"  ;
+//
+//                                }else{
+//                                    echo '------ not in'.$json->f_log_name."<br/>"."<br/>"  ;
+//                                }
+//
+//                            } else {
+//                                $logType[$json->f_log_name]++;
+//                            }
                         } else {
 //                            echo $v;
                             echo "<br/>";
@@ -119,22 +134,87 @@ class SiteController extends Controller
                     }
                 }
 
-
             }
         }
-        foreach($ret as $v){
-//            echo $v[1]."   :  " .$v[0]."<br/>";
-            echo $v[0] ;
-            echo "<br/>"; echo "<br/>";
-        }
-//        foreach($type as $v){
+//        foreach($ret as $v){
 ////            echo $v[1]."   :  " .$v[0]."<br/>";
-//            echo $v ;
+//            echo $v[0] ;
 //            echo "<br/>"; echo "<br/>";
 //        }
+////        foreach($type as $v){
+//////            echo $v[1]."   :  " .$v[0]."<br/>";
+////            echo $v ;
+////            echo "<br/>"; echo "<br/>";
+////        }
 //        print_r($logType) ;
         closedir($current_dir);
     }
+
+    public function createTab($object,$tabName){
+        //对象先转换成为数据
+        $data = $this->objeToArr($object) ;
+        var_dump($data) ;die;
+
+    }
+
+    public function objeToArr($object){
+        $array=array();
+        if (is_object($object)) {
+            foreach ($object as $key => $value) {
+                if(is_object($value)){
+                    if($value) {
+                        $value = $this->objeToArr($value);
+                    }
+                }
+                $array[$key] = $value;
+
+            }
+        }
+        else {
+
+            $array = $object;
+        }
+
+//        print_r($array) ;die;
+        return $array;
+
+    }
+
+    /**
+     * @param $tmpData 原始数据 是个二维数据
+     *
+     *
+     * @param $ret      一维数据 数据表要插入的数据字段值
+     *
+     */
+    public function createSqlData($tmpData){
+        $tabName = $tmpData['f_log_name'];
+        $sql = "DESC bi_$tabName " ;
+        $command=Yii::$app->db->createCommand($sql);
+        $columns = $command->queryAll();
+        foreach($columns as $v){
+            $useColu[] =$v['Field'] ;
+        }
+
+        foreach($tmpData as $key=>$val){
+            if($key=='f_log_name') continue ;
+            if(is_array($val)){
+                foreach($val as $k1=> $v1){
+                    if(!in_array($k1,$useColu)){
+
+                        echo $tabName.":".$k1."<br/><br/>" ;
+                    }
+                }
+            }else{
+                if(!in_array($key,$useColu)){
+                    echo  $tabName.":".$key."<br/><br/>" ;
+                }
+            }
+        }
+
+    }
+
+
 
 
 }
