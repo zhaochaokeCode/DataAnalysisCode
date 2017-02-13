23d6be508645ab576e7afc02e1b7030fd45f3394
Zhaochaoke <308471666@qq.com>
2017-02-13 14:53:31 +0800

    test

--- frontend/controllers/PayController.php
+++ frontend/controllers/PayController.php
@@ -13,6 +13,7 @@ class PayController extends Controller
 
     }
     public function actionCreateorder(){
+        var_dump($_POST) ;
         if($_POST){
             echo file_put_contents('/tmp/data.txt',json_encode($_POST)."\n",FILE_APPEND) ;
         }
