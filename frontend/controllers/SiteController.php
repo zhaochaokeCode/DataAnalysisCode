080a9e4a599b6deb9382549addf9aa1b11c4f1d1
Zhaochaoke <308471666@qq.com>
2017-02-16 20:47:23 +0800

    tetg

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -127,6 +127,9 @@ class SiteController extends CommController
                 $tabName = 'bi_'.$k ;
                 $keyStr = implode(',',$keyData[$k]) ;
                 foreach($v as $v3){
+                    if($tabName=='bi_log_online_character_cnt '){
+                        var_dump($v3) ;die;
+                    }
                     foreach($v3 as $k4=>$v4){
                         if($v4!='null'||$v4!='default')
                         $v3[$k4] ="'$v4'";
