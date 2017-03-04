4ceb7d6e5e15862fe5b1001bc7878966f091548e
Zhaochaoke <308471666@qq.com>
2017-03-04 19:00:38 +0800

    sdf

--- frontend/web/test.php
+++ frontend/web/test.php
@@ -1,15 +1,86 @@
 <?php
-/**
- *
- * Created by PhpStorm.
- * User: zhaochaoke
- * Date: 16/12/30
- * Time: 上午11:46
- */
+
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
+class mongdb{
+    public $table ;
+
+    public function __construct(){
+        $conn  = new MongoClient('mongodb://root:cthy8566@dds-m5ea0bb2c4c1c5f41.mongodb.rds.aliyuncs.com:3717');
+        $this->table  = $conn->user_info->user_detail ;
+        $this->selectFun() ;
+    }
+
+    public function selectFun(){
+        $data  = $_POST ;
+
+        switch($data['action']){
+            case 'regist' :
+                $this->regist($data) ;
+                break ;
+            case 'login' :
+                $this->login($data) ;
+                break ;
+        }
+
+    }
+
+
+    public function regist($data){
+
+        $info = array('name'=>$data['name']) ;
+        $userInfo = $this->table->findone($info) ;
+        if($userInfo) $this->retdata(401,'fail, user already exit') ;
+
+        $code = rand(1000,9000).time() ;
+        $data2 = array(
+            "name"=>$data['name'],
+            "password"=>md5($data['password'].$code),
+            "code"=>$code,
+            "regist_time"=>$data['regist_time']
+        ) ;
+        if($data['email']){
+            $data2['email']= $data['email'] ;
+        }
+        $result = $this->table->insert($data2) ;
+        if($result['ok']==1){
+            $tmp = '$id' ;
+            $retId = $data2['_id']->$tmp ;
+            $this->retdata(200,$retId) ;
+        }else{
+            $this->retdata(400,'fail') ;
+        }
+
+
+    }
+
+    public function login($data){
+        $info = array('name'=>$data['name']) ;
+        $userInfo = $this->table->findone($info) ;
+
+        if($userInfo){
+            $sign = md5($data['password'].$userInfo['code']) ;
+            if($sign==$userInfo['password']){
+                $this->retdata(200,'success') ;
+            }
+        }
+        $this->retdata(400,'fail') ;
+
+    }
+
+
+    public function retdata($code,$info){
+
+        $data = array('code'=>$code,'id'=>$info) ;
+        echo json_encode($data) ;
+        die;
+    }
+}
+$newClass = new mongdb() ;
 
 
 
-$str = 'order_id=2017030321001004280223720066other=1time=2017-03-0315:40:33total_money=0.01EoL32&JSUVt30JHir6v48sk!' ;
 
-$str = 'order_id=2017030321001004280223724368other=1time=2017-03-0315:56:36total_money=0.01EoL32&JSUVt30JHir6v48sk!' ;
-echo md5($str);
+//$table->insert($user);
+//$data = $table->find(array('name' => 'caleng')) ;
+
