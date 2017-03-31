435099f19d3f849173e2a18922119de436872d09
Zhaochaoke <308471666@qq.com>
2017-03-31 17:02:28 +0800

    tewst

--- frontend/controllers/CheckdataController.php
+++ frontend/controllers/CheckdataController.php
@@ -27,7 +27,7 @@ class CheckdataController extends Controller
         foreach($res as $k=>$v){
             rsort($v) ;
 
-            $data =file_get_contents($k."-".$v[0]) ;
+            $data =file_get_contents($dir."/".$k."-".$v[0]) ;
             var_dump($data) ;
             die;
         }
