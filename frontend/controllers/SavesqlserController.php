8e0df43da80f73552ea54884b03c4b185396dbaf
Zhaochaoke <308471666@qq.com>
2017-04-20 15:04:22 +0800

    sd

--- frontend/controllers/SavesqlserController.php
+++ frontend/controllers/SavesqlserController.php
@@ -297,7 +297,8 @@ class SavesqlserController extends Controller
                 }
                 if ($valStr) {
                     $sql = "INSERT INTO $tabName ($keyStr)  VALUES $valStr ";
-                    $tabArr = $this->mssdb->runSql($sql);
+                    echo $sql ;
+//                    $tabArr = $this->mssdb->runSql($sql);
                     sleep(0.002);
                 }
             }
